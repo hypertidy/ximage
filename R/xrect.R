44c124ffbd962774f6a08be2f3418893a15162db
Michael Sumner <mdsumner@gmail.com>
2022-09-08 00:05:14 +1000

    xrect

---
+++ R/xrect.R
@@ -0,0 +1,29 @@
+#' xrect
+#'
+#' Draw rectangles from four colummns xmin,xmax,ymin,ymax
+#'
+#' Calls [rect()], but will instantiate a plot if `add = FALSE`.
+#'
+#' @param x four columns worth of rectangles
+#' @param add instantiate a plot or add to existing (default is `add = FALSE`)
+#' @param ... arguments passed to [rect()]
+#' @param asp aspect ratio, defaults to 1
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' xrect(runif(100))
+#' xrect(sort(runif(100)))
+#' xrect(runif(100), col = hcl.colors(25, alpha = seq(.2, .8, length.out = 25)))
+xrect <- function(x, add = FALSE, ..., asp = 1L) {
+  ## assuming cbind(xmin, xmax, ymin, ymax)
+  x <- matrix(unlist(x, use.names = FALSE), ncol = 4L)
+  xlim <- range(x[,1:2], na.rm = TRUE)
+  ylim <- range(x[,3:4], na.rm = TRUE)
+  if (!add) {
+    plot(NA, xlim = xlim, ylim = ylim, asp = asp, xlab = "", ylab = "")
+  }
+  rect(x[,1L], x[,3L], x[,2L], x[, 4L], ...)
+ invisible(NULL)
+}
